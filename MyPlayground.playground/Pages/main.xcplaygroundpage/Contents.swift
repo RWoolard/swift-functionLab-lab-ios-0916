04daada91a4846d6e48579b763e8d8b94aa5bf40
RWoolard <r.woolard456@gmail.com>
2016-08-29 18:03:10 -0400

    Done

--- MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
+++ MyPlayground.playground/Pages/main.xcplaygroundpage/Contents.swift
@@ -12,6 +12,11 @@
  ### 1. Write a function called `helloWorld()` that prints "Hello, world!" to the console. Then call it to see your string printed to the playground console.
  */
 // write your code here
+func helloWorld() {
+    print("Hello, world!")
+}
+
+helloWorld()
 
 
 
@@ -20,7 +25,12 @@
  ### 2. Write your own function in which you declare a constant inside the function's body and then print that constant to the console. Call this function to see your string printed to the playground console.
  */
 // write your code here
+func thisIsMyPersonalFunction() {
+    let myMotto = "Learn, Love, Code"
+    print(myMotto)
+}
 
+thisIsMyPersonalFunction()
 
 
 
@@ -28,7 +38,14 @@
  ### 3. Write a function that takes a person's name as an argument and prints a greeting to the console. Call it several times with different arguments. What do you think you'll see in the console?
  */
 // write your code here
-
+func myPersonalizedGreeting(personsName: String){
+    
+    print("Hey! Whats Up \(personsName)?!")
+    
+}
+myPersonalizedGreeting("Ben")
+myPersonalizedGreeting("Victoria")
+myPersonalizedGreeting("Michelle")
 
 
 
@@ -37,14 +54,37 @@
  */
 // write your code here
 
+// I expect to see the value of the variable passed through my function because its still a string. 
 
+var newName = "Chris"
+
+myPersonalizedGreeting(newName)
+
+newName = "John"
+
+myPersonalizedGreeting(newName)
+
+// The new value of my variable.
 
 
 /*: question5
  ### 5. Write your own function in which you declare a _variable_ (of any type) inside the function's body. Print out this variable to the console from within your function. After you print the variable once, assign it to a new variable on the next line. Print it again (after the line on which you assign it to a new value). Call your function several times. What do you expect to see printed to the playground's console each time you call this function?
  */
 // write your code here
+func myTest(){
+    var myFavoriteColor = "Red"
+    print(myFavoriteColor)
+    myFavoriteColor = "Blue"
+    print(myFavoriteColor)
+    myFavoriteColor = "Green"
+    print(myFavoriteColor)
+}
 
+myTest()
+myTest()
+myTest()
+
+// I expected to see all of the changes to my favorite colors with every call to print.
 
 
 
